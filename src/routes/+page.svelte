69e47ce841e6d5def99b3a118bacd21806dc9d9e
Dimuthu Attanayake <dimuthuc.attanayake@gmail.com>
2026-06-10 15:03:52 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  // logic and data lives here
+</script>
+
+<!-- markup lives here -->
+ Welcome to <b>Data, Computation & Innovation II</b>
+
+<style>
+  /* styles live here */
+</style>
