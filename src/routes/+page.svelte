31ed25ddf755c4977718dc2be0e9f2a06ffa066c
DimuthuAttanayake <dimuthuc.attanayake@gmail.com>
2026-06-23 20:05:09 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,6 +28,18 @@ const yScale = d3.scaleLinear()
  //4.generators (if needed)
 //const LineGenerator =d3.line().x(...).y(...)
 
+const lineGenerator = d3.Line()
+  .x(d => xScale(d.month))
+  .y(d => yScale(d3.temp));
+
+const areaGenerator = d3.area()
+  .x(d => xScale(d.month))
+  .y0(height - margin.bottom) //baseline
+  .y1((d) => yScale(d.temp)); // line
+
+console.log(lineGenerator(data))   // this is like the print in python
+
+
 //5.rendering
 
 
@@ -37,6 +49,109 @@ const yScale = d3.scaleLinear()
 
 
 <svg viewBox="0 0 {width} {height}" {width} {height}>
+
+       {#each yScale.ticks(5) as tick} //adding y axis
+            <line
+              x1={margin.left}
+              y1={yScale(tick)}
+              x2={width -margin.right} //to adjust the margins
+              y2={yScale(tick)}
+              stroke="#999"
+              />
+
+              <text
+              x1={margin.left}
+              y1={yScale(tick)}
+              text-anchor = "end"
+              dominant baseline= "middle"
+              font size ="12"
+              fill = #666
+              >
+               {tick}º //to add a degree sign at each point (option + 0)
+            </text>           
+       {/each}
+
+
+
+       {#each data as d}
+
+              <text // adding axis labels
+              x={xScale(d.month)}
+              y={height - margin.bottom +5}
+              dominant baseline= "hanging"
+              font size ="12"
+              fill = #666
+              text-anchor = "middle"
+
+              >
+               {d.month}
+            </text>   
+            
+          
+ 
+           {/each}
+
+         <!-- x axis line -->
+	       <line
+		       x1={margin.left}
+		       x2={width - margin.right}
+		       y1={height - margin.bottom}
+		       y2={height - margin.bottom}
+		       stroke="#000"
+	        />
+
+	       <!-- y axis line -->
+	      <line
+		     x1={margin.left}
+		     x2={margin.left}
+		     y1={margin.top}
+		     y2={height - margin.bottom}
+         stroke="black"
+	      />
+
+	 <path d={areaGenerator(data)} fill="steelblue" opacity="0.15" />
+
+	 <path d={lineGenerator(data)} fill="none" stroke="steelblue" stroke-width="2" />
+
+   {#each data as d}
+		<circle
+			cx={xScale(d.month)}
+			cy={yScale(d.temp)}
+			r="4"
+			fill="white"
+			stroke="steelblue"
+			stroke-width="2"
+		/>
+	{/each}
+  </svg>
+
+
+  <path // adding the area
+      d={lineGenerator(data)}
+      fill ="none"
+      stroke ="purple"
+      stroke-width = "2"
+  />  
+  
+  <path // adding the line
+      d={areaGenerator(data)}
+      fill ="purple"
+      opacity = "0.15"
+  />  
+
+{#each data as d}
+     
+      <circle
+           cx ={xScale(d.month)}
+           cy = {yScale(d.temp)}
+           r='4'
+           fill= "white"
+           stroke = "purple"
+           stroke-width ="2"
+      />
+
+{each/}
+
 {#each data as d, i}
 <rect
 x={margin.left + i * 100}
